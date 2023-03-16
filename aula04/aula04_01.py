db8f102f16cdb24d9be61a4e698d77b68209f4f6
Prof. Hebert <hebertphp@gmail.com>
2023-03-16 13:21:11 -0300

    16-03-23

---
+++ aula04/aula04_01.py
@@ -0,0 +1,8 @@
+#Operadores aritméticos
+x = int(input("Digite o primeiro número "))
+y = int(input("Digite o segundo número "))
+print("Soma:",x+y)
+print("Subtração:",x-y)
+print("Multiplicação:",x*y)
+print("Divisão:",x/y)
+print("Divisão inteira:",x//y)
